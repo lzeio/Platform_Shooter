1875ff60e422745e69d9c00470282fceb9dad797
Labham Baheti <49443114+lzeio@users.noreply.github.com>
2022-01-29 12:25:30 +0530

    Player and Game Scene Controller Added
    Level has been added

---
+++ Platform_Shooter/Assets/Scripts/GameSceneController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneController : MonoBehaviour
+{
+    [SerializeField]
+    PlayerController player;
+    [SerializeField]
+    Camera Cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Platform_Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    float jumpForce,moveForce;
+    [SerializeField]
+    LayerMask groundMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+        Debug.Log(Grounded());
+        
+    }
+
+    void PlayerInput()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(-moveForce * Time.deltaTime, this.GetComponent<Rigidbody>().velocity.y, this.GetComponent<Rigidbody>().velocity.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<Rigidbody>().velocity = new Vector3(moveForce * Time.deltaTime, this.GetComponent<Rigidbody>().velocity.y, this.GetComponent<Rigidbody>().velocity.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&&Grounded())
+        {
+       
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    bool Grounded()
+    {
+        bool isgrounded = Physics.CheckSphere(transform.position, 0.5f, groundMask);
+        return isgrounded;
+    }
+}
