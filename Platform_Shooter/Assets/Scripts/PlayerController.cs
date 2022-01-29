d37a44b68286a8bc4615682c09273a29c819de93
Labham Baheti <49443114+lzeio@users.noreply.github.com>
2022-01-29 16:52:18 +0530

    Update PlayerController.cs

--- Platform_Shooter/Assets/Scripts/PlayerController.cs
+++ Platform_Shooter/Assets/Scripts/PlayerController.cs
@@ -49,7 +49,7 @@ public class PlayerController : MonoBehaviour
             this.GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0));
         }
 
-        if(Input.GetMouseButton(0)
+        if(Input.GetMouseButtonDown(0))
         {
             GameObject bulletObject = GameObject.Instantiate<GameObject>(bulletPrefab);
             bulletObject.transform.position = new Vector3(this.transform.position.x + direction, this.transform.position.y, this.transform.position.z);
